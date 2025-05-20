654960824ce3726f78a2acf5ef8734d5fe5efe5a
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-20 15:28:32 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Index template.
+ *
+ * Loading this template on a website should be avoided, unless a plugin is misbehaving
+ * or there is an abnormal situation. However, as a fallback measure in case this file
+ * is loaded, the following code will correctly load our `public/view/index.php` template.
+ */
+
+// Access the view template engine.
+$engine = Backdrop\App::resolve( 'view/engine' );
+
+// Load the index template.
+$engine->display( 'index' );
