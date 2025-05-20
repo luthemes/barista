6533b2e85ba8e88ba102c787f592b316f58b2bf3
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-20 16:17:03 +0000

    initial commit

---
+++ app/functions-template.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template tags.
+ *
+ * This file holds template tags for the theme. Template tags are PHP functions
+ * meant for use within theme templates.
+ *
+ * @package   Barista
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/barista
+ */
+
+namespace Barista;
+
+/**
+ * Returns the metadata separator.
+ *
+ * @since  0.0.1
+ * @access public
+ * @param  string  $sep  String to separate metadata.
+ * @return string
+ */
+function sep( $sep = '' ) {
+
+    return apply_filters( 'barista/sep', sprintf(
+        ' <span class="sep">%s</span> ',
+        $sep ?: esc_html_x( '&middot;', 'meta separator', 'barista' )
+    ) );
+}
